6edd10caace8ee50787cfecf6ac708d73d6b034f
laggykiller <61652821+laggykiller@users.noreply.github.com>
2020-11-19 04:19:03 +0800

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,2 +1,8 @@
 @echo off
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
 python MoticDownloader.py
+goto:eof
+:errorNoPython
+echo.
+echo Error^: Python not installed. Please install from https://www.python.org/downloads
