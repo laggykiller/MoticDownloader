baa4d6eb2feb6dd0c2d34707ada208e8ff1e1c59
laggykiller <61652821+laggykiller@users.noreply.github.com>
2020-12-23 19:05:11 +0800

    Delete windows_launch.bat

--- windows_launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python --version 3>NUL
-if errorlevel 1 goto errorNoPython
-python MoticDownloader.py
-goto:eof
-:errorNoPython
-echo.
-echo Error^: Python not installed. Please install from https://www.python.org/downloads
-pause
